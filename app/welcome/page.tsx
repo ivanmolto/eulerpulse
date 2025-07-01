3f9a0347fd73585a53b8d623cc4c47e493ebdd67
Ivan Molto <ivanmolto@gmail.com>
2025-07-01 23:04:01 +0200

    feat: Add Home page with Hero component for enhanced user experience

---
+++ app/welcome/page.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "@/components/ui/Hero";
+
+export default function Home() {
+  return (
+    <main className="relative mx-auto flex flex-col">
+      <div className="pt-56">
+        <Hero />
+      </div>
+    </main>
+  );
+}
