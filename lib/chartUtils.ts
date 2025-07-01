e9a385d812dac56784d8eac5626bbf35389f43da
Ivan Molto <ivanmolto@gmail.com>
2025-07-01 17:25:09 +0200

    feat: Implement chart utility functions and color definitions for enhanced chart styling

---
+++ lib/chartUtils.ts
@@ -0,0 +1,224 @@
+export type ColorUtility = "bg" | "stroke" | "fill" | "text";
+
+export const chartColors = {
+  sky: {
+    bg: "bg-sky-500 dark:bg-sky-500",
+    stroke: "stroke-sky-500 dark:stroke-sky-500",
+    fill: "fill-sky-500 dark:fill-sky-500",
+    text: "text-sky-500 dark:text-sky-500",
+  },
+  blue: {
+    bg: "bg-blue-500 dark:bg-blue-500",
+    stroke: "stroke-blue-500 dark:stroke-blue-500",
+    fill: "fill-blue-500 dark:fill-blue-500",
+    text: "text-blue-500 dark:text-blue-500",
+  },
+  lightBlue: {
+    bg: "bg-blue-300/50 dark:bg-blue-800/50",
+    stroke: "stroke-blue-300/50 dark:stroke-blue-800/50",
+    fill: "fill-blue-300/50 dark:fill-blue-800/50",
+    text: "text-blue-300/50 dark:text-blue-800/50",
+  },
+  emerald: {
+    bg: "bg-emerald-500 dark:bg-emerald-400",
+    stroke: "stroke-emerald-500 dark:stroke-emerald-400",
+    fill: "fill-emerald-500 dark:fill-emerald-400",
+    text: "text-emerald-500 dark:text-emerald-400",
+  },
+  lightEmerald: {
+    bg: "bg-emerald-300/50 dark:bg-emerald-800/50",
+    stroke: "stroke-emerald-300/50 dark:stroke-emerald-800/50",
+    fill: "fill-emerald-300/50 dark:fill-emerald-800/50",
+    text: "text-emerald-300/50 dark:text-emerald-800/50",
+  },
+  violet: {
+    bg: "bg-violet-500 dark:bg-violet-500",
+    stroke: "stroke-violet-500 dark:stroke-violet-500",
+    fill: "fill-violet-500 dark:fill-violet-500",
+    text: "text-violet-500 dark:text-violet-500",
+  },
+  lightViolet: {
+    bg: "bg-violet-300/50 dark:bg-violet-800/50",
+    stroke: "stroke-violet-300/50 dark:stroke-violet-800/50",
+    fill: "fill-violet-300/50 dark:fill-violet-800/50",
+    text: "text-violet-300/50 dark:text-violet-800/50",
+  },
+  amber: {
+    bg: "bg-amber-400 dark:bg-amber-400",
+    stroke: "stroke-amber-400 dark:stroke-amber-400",
+    fill: "fill-amber-400 dark:fill-amber-400",
+    text: "text-amber-400 dark:text-amber-400",
+  },
+  yellow: {
+    bg: "bg-yellow-300 dark:bg-yellow-300",
+    stroke: "stroke-yellow-300 dark:stroke-yellow-300",
+    fill: "fill-yellow-300 dark:fill-yellow-300",
+    text: "text-yellow-300 dark:text-yellow-300",
+  },
+  lightYellow: {
+    bg: "bg-yellow-300/50 dark:bg-yellow-800/50",
+    stroke: "stroke-yellow-300/50 dark:stroke-yellow-800/50",
+    fill: "fill-yellow-300/50 dark:fill-yellow-800/50",
+    text: "text-yellow-300/50 dark:text-yellow-800/50",
+  },
+  gray: {
+    bg: "bg-gray-400 dark:bg-gray-600",
+    stroke: "stroke-gray-400 dark:stroke-gray-600",
+    fill: "fill-gray-400 dark:fill-gray-600",
+    text: "text-gray-400 dark:text-gray-600",
+  },
+  rose: {
+    bg: "bg-rose-600 dark:bg-rose-500",
+    stroke: "stroke-rose-600 dark:stroke-rose-500",
+    fill: "fill-rose-600 dark:fill-rose-500",
+    text: "text-rose-600 dark:text-rose-500",
+  },
+  cyan: {
+    bg: "bg-cyan-500 dark:bg-cyan-500",
+    stroke: "stroke-cyan-500 dark:stroke-cyan-500",
+    fill: "fill-cyan-500 dark:fill-cyan-500",
+    text: "text-cyan-500 dark:text-cyan-500",
+  },
+  indigo: {
+    bg: "bg-indigo-600 dark:bg-indigo-500",
+    stroke: "stroke-indigo-600 dark:stroke-indigo-500",
+    fill: "fill-indigo-600 dark:fill-indigo-500",
+    text: "text-indigo-600 dark:text-indigo-500",
+  },
+  orange: {
+    bg: "bg-orange-500 dark:bg-orange-400",
+    stroke: "stroke-orange-500 dark:stroke-orange-400",
+    fill: "fill-orange-500 dark:fill-orange-400",
+    text: "text-orange-500 dark:text-orange-400",
+  },
+  pink: {
+    bg: "bg-pink-500 dark:bg-pink-500",
+    stroke: "stroke-pink-500 dark:stroke-pink-500",
+    fill: "fill-pink-500 dark:fill-pink-500",
+    text: "text-pink-500 dark:text-pink-500",
+  },
+  red: {
+    bg: "bg-red-500 dark:bg-red-500",
+    stroke: "stroke-red-500 dark:stroke-red-500",
+    fill: "fill-red-500 dark:fill-red-500",
+    text: "text-red-500 dark:text-red-500",
+  },
+  lime: {
+    bg: "bg-lime-500 dark:bg-lime-500",
+    stroke: "stroke-lime-500 dark:stroke-lime-500",
+    fill: "fill-lime-500 dark:fill-lime-500",
+    text: "text-lime-500 dark:text-lime-500",
+  },
+  lightGreen: {
+    bg: "bg-green-300/50 dark:bg-green-800/50",
+    stroke: "stroke-green-300/50 dark:stroke-green-800/50",
+    fill: "fill-green-300/50 dark:fill-green-800/50",
+    text: "text-green-300/50 dark:text-green-800/50",
+  },
+  green: {
+    bg: "bg-green-500 dark:bg-green-500",
+    stroke: "stroke-green-500 dark:stroke-green-500",
+    fill: "fill-green-500 dark:fill-green-500",
+    text: "text-green-500 dark:text-green-500",
+  },
+  teal: {
+    bg: "bg-teal-500 dark:bg-teal-500",
+    stroke: "stroke-teal-500 dark:stroke-teal-500",
+    fill: "fill-teal-500 dark:fill-teal-500",
+    text: "text-teal-500 dark:text-teal-500",
+  },
+  lightGray: {
+    bg: "bg-gray-300 dark:bg-gray-700",
+    stroke: "stroke-gray-300 dark:stroke-gray-700",
+    fill: "fill-gray-300 dark:fill-gray-700",
+    text: "text-gray-300 dark:text-gray-700",
+  },
+  zinc: {
+    bg: "bg-zinc-500 dark:bg-zinc-500",
+    stroke: "stroke-zinc-500 dark:stroke-zinc-500",
+    fill: "fill-zinc-500 dark:fill-zinc-500",
+    text: "text-zinc-500 dark:text-zinc-500",
+  },
+  neutral: {
+    bg: "bg-neutral-500 dark:bg-neutral-500",
+    stroke: "stroke-neutral-500 dark:stroke-neutral-500",
+    fill: "fill-neutral-500 dark:fill-neutral-500",
+    text: "text-neutral-500 dark:text-neutral-500",
+  },
+  purple: {
+    bg: "bg-purple-500 dark:bg-purple-500",
+    stroke: "stroke-purple-500 dark:stroke-purple-500",
+    fill: "fill-purple-500 dark:fill-purple-500",
+    text: "text-purple-500 dark:text-purple-500",
+  },
+  sync: {
+    bg: "bg-gray-950 dark:bg-gray-50",
+    stroke: "stroke-gray-950 dark:stroke-gray-50",
+    fill: "fill-gray-950 dark:fill-gray-50",
+    text: "text-gray-950 dark:text-gray-50",
+  },
+} as const satisfies {
+  [color: string]: {
+    [key in ColorUtility]: string;
+  };
+};
+
+export type AvailableChartColorsKeys = keyof typeof chartColors;
+
+export const AvailableChartColors: AvailableChartColorsKeys[] = Object.keys(
+  chartColors
+) as Array<AvailableChartColorsKeys>;
+
+export const constructCategoryColors = (
+  categories: string[],
+  colors: AvailableChartColorsKeys[]
+): Map<string, AvailableChartColorsKeys> => {
+  const categoryColors = new Map<string, AvailableChartColorsKeys>();
+  categories.forEach((category, index) => {
+    categoryColors.set(category, colors[index % colors.length]);
+  });
+  return categoryColors;
+};
+
+export const getColorClassName = (
+  color: AvailableChartColorsKeys,
+  type: ColorUtility
+): string => {
+  const fallbackColor = {
+    bg: "bg-gray-500",
+    stroke: "stroke-gray-500",
+    fill: "fill-gray-500",
+    text: "text-gray-500",
+  };
+  return chartColors[color]?.[type] ?? fallbackColor[type];
+};
+
+export const getYAxisDomain = (
+  autoMinValue: boolean,
+  minValue: number | undefined,
+  maxValue: number | undefined
+) => {
+  const minDomain = autoMinValue ? "auto" : minValue ?? 0;
+  const maxDomain = maxValue ?? "auto";
+  return [minDomain, maxDomain];
+};
+
+export function hasOnlyOneValueForKey(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  array: any[],
+  keyToCheck: string
+): boolean {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const val: any[] = [];
+
+  for (const obj of array) {
+    if (Object.prototype.hasOwnProperty.call(obj, keyToCheck)) {
+      val.push(obj[keyToCheck]);
+      if (val.length > 1) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
