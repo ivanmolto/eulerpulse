7e3c24c5ca0a245de00152072dc47d5573566350
Ivan Molto <ivanmolto@gmail.com>
2025-07-07 00:45:06 +0200

    feat: Add Overview component for displaying cumulative Unichain fees over time

---
+++ app/(dashboard)/unichain-fees/overview/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import UnichainCumulativeFees from "@/components/ui/unichain-fees/UnichainCumulativeFees";
+import { cx } from "@/lib/utils";
+
+export default function Overview() {
+  return (
+    <section aria-labelledby="usage-overview">
+      <dl
+        className={cx(
+          "ml-2 mt-10 grid grid-cols-1 gap-14 sm:grid-cols-1 md:grid-cols-1 xl:grid-cols-1"
+        )}
+      >
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Cumulative fees over time on Unichain
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              Fees accumulated over time on Unichain
+            </dd>
+          </div>
+          <UnichainCumulativeFees slug="unichain-cumulative-fees" />
+        </div>
+      </dl>
+    </section>
+  );
+}
