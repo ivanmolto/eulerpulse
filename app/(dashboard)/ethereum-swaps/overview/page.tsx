9e7855ccf35680c493bf00c72021f8dcb0bd5b69
Ivan Molto <ivanmolto@gmail.com>
2025-07-06 23:12:25 +0200

    feat: Create Overview page for Ethereum swaps, displaying cumulative swaps data and descriptions

---
+++ app/(dashboard)/ethereum-swaps/overview/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import EthereumCumulativeSwaps from "@/components/ui/ethereum-swaps/EthereumCumulativeSwaps";
+import { cx } from "@/lib/utils";
+
+export default function Overview() {
+  return (
+    <section aria-labelledby="usage-overview">
+      <dl
+        className={cx(
+          "ml-2 mt-10 grid grid-cols-1 gap-14 sm:grid-cols-1 md:grid-cols-1 xl:grid-cols-1"
+        )}
+      >
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Cumulative swaps over time on Ethereum
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              Total number of swaps accumulated over time on Ethereum
+            </dd>
+          </div>
+          <EthereumCumulativeSwaps slug="ethereum-cumulative-swaps" />
+        </div>
+      </dl>
+    </section>
+  );
+}
