4718ede8a7fcd4b5678cf6d2f48d988c1b2bb972
Ivan Molto <ivanmolto@gmail.com>
2025-07-07 00:45:37 +0200

    feat: Create layout for Unichain fees dashboard with navigation and metrics cards

---
+++ app/(dashboard)/unichain-fees/layout.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { TabNavigation, TabNavigationLink } from "@/components/TabNavigation";
+import { UnichainFeesMetricsCards } from "@/components/ui/unichain-fees/UnichainFeesMetricsCards";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { siteConfig } from "../../siteConfig";
+
+const navigation = [
+  { name: "Overview", href: siteConfig.baseLinks.unichainfees.overview },
+  { name: "Trends", href: siteConfig.baseLinks.unichainfees.trends },
+];
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+  return (
+    <>
+      <div className="bg-white dark:bg-gray-925">
+        <div className="p-4 sm:p-2">
+          <UnichainFeesMetricsCards />
+        </div>
+        <TabNavigation className="mt-6 gap-x-4 px-4 sm:px-6">
+          {navigation.map((item) => (
+            <TabNavigationLink
+              key={item.name}
+              asChild
+              active={pathname === item.href}
+            >
+              <Link href={item.href}>{item.name}</Link>
+            </TabNavigationLink>
+          ))}
+        </TabNavigation>
+        <>{children}</>
+      </div>
+    </>
+  );
+}
