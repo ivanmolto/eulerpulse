9fdd038776bcd5efbde20b17cfbece9affc1f3cb
Ivan Molto <ivanmolto@gmail.com>
2025-07-07 11:58:24 +0200

    feat: Introduce VolumesMetricsCards component to display volume overview with VolumesCount

---
+++ components/ui/all-volumes/VolumesMetricsCards.tsx
@@ -0,0 +1,17 @@
+import VolumesCount from "@/components/ui/all-volumes/VolumesCount";
+
+export function VolumesMetricsCards() {
+  return (
+    <>
+      <h1 className="text-lg font-semibold text-gray-900 dark:text-gray-50">
+        Overview
+      </h1>
+      <dl className="mt-6 flex flex-wrap items-center gap-x-12 gap-y-8">
+        <div>
+          <dt className="text-sm text-gray-500 dark:text-gray-500">Volume</dt>
+          <VolumesCount slug="all-volumes-count" />
+        </div>
+      </dl>
+    </>
+  );
+}
