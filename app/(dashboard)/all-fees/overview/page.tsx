3d4574a84efce684155f1e21ecc41dbcb2aebe90
Ivan Molto <ivanmolto@gmail.com>
2025-07-07 10:26:54 +0200

    feat: Add Overview page for displaying cumulative fees and staked fees by chain

---
+++ app/(dashboard)/all-fees/overview/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import CumulativeFees from "@/components/ui/all-fees/CumulativeFees";
+import CumulativeStakedFees from "@/components/ui/all-fees/CumulativeStakedFees";
+import CumulativeFeesChain from "@/components/ui/all-fees/CumulativeFeesChain";
+import { cx } from "@/lib/utils";
+
+export default function Overview() {
+  return (
+    <>
+      <section aria-label="Overview">
+        <dl className="-ml-4 grid grid-cols-1 gap-x-14 gap-y-10 border-t border-gray-200 p-6 md:grid-cols-2 dark:border-gray-800">
+          <div className="flex flex-col justify-between p-0">
+            <div>
+              <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+                Cumulative Fees over time
+              </dt>
+              <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+                Fees accumulated over time
+              </dd>
+            </div>
+            <CumulativeFees slug="cumulative-fees" />
+          </div>
+          <div className="flex flex-col justify-between p-0">
+            <div>
+              <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+                Cumulative Fees over time by chain
+              </dt>
+              <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+                Fees over time by chain
+              </dd>
+            </div>
+            <CumulativeFeesChain slug="cumulative-fees-chain" />
+          </div>
+        </dl>
+      </section>
+      <section aria-labelledby="overview">
+        <dl
+          className={cx(
+            "ml-2 mt-10 grid grid-cols-1 gap-14 sm:grid-cols-1 md:grid-cols-1 xl:grid-cols-1"
+          )}
+        >
+          <div className="flex flex-col justify-between p-0">
+            <div>
+              <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+                Cumulative Fees over time, staked by chain
+              </dt>
+              <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+                Fees accumulated over time, staked by chain
+              </dd>
+            </div>
+            <CumulativeStakedFees slug="cumulative-fees-chain" />
+          </div>
+        </dl>
+      </section>
+    </>
+  );
+}
