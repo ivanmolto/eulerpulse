372bbbde485dcbd3833070a9bef348f9c2591e01
Ivan Molto <ivanmolto@gmail.com>
2025-07-07 12:00:46 +0200

    feat: Implement Overview page for displaying cumulative volumes and staked volumes by chain

---
+++ app/(dashboard)/all-volumes/overview/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import CumulativeVolumes from "@/components/ui/all-volumes/CumulativeVolumes";
+import CumulativeStakedVolumes from "@/components/ui/all-volumes/CumulativeStakedVolumes";
+import CumulativeVolumesChain from "@/components/ui/all-volumes/CumulativeVolumesChain";
+import { cx } from "@/lib/utils";
+
+export default function Overview() {
+  return (
+    <>
+      <section aria-label="Overview">
+        <dl className="-ml-4 grid grid-cols-1 gap-x-14 gap-y-10 border-t border-gray-200 p-6 md:grid-cols-2 dark:border-gray-800">
+          <div className="flex flex-col justify-between p-0">
+            <div>
+              <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+                Cumulative Volume over time
+              </dt>
+              <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+                Volume accumulated over time
+              </dd>
+            </div>
+            <CumulativeVolumes slug="cumulative-volumes" />
+          </div>
+          <div className="flex flex-col justify-between p-0">
+            <div>
+              <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+                Cumulative Volume over time by chain
+              </dt>
+              <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+                Volume over time by chain
+              </dd>
+            </div>
+            <CumulativeVolumesChain slug="cumulative-volumes-chain" />
+          </div>
+        </dl>
+      </section>
+      <section aria-labelledby="usage-overview">
+        <dl
+          className={cx(
+            "ml-2 mt-10 grid grid-cols-1 gap-14 sm:grid-cols-1 md:grid-cols-1 xl:grid-cols-1"
+          )}
+        >
+          <div className="flex flex-col justify-between p-0">
+            <div>
+              <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+                Cumulative Volume over time, staked by chain
+              </dt>
+              <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+                Volume accumulated over time, staked by chain
+              </dd>
+            </div>
+            <CumulativeStakedVolumes slug="cumulative-volumes-chain" />
+          </div>
+        </dl>
+      </section>
+    </>
+  );
+}
