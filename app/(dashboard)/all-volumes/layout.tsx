448e9bfdc67547d853a37e80a5b40c9fdc8714eb
Ivan Molto <ivanmolto@gmail.com>
2025-07-07 12:00:10 +0200

    feat: Create layout for All Volumes dashboard with navigation and metrics cards

---
+++ app/(dashboard)/all-volumes/layout.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { TabNavigation, TabNavigationLink } from "@/components/TabNavigation";
+import { VolumesMetricsCards } from "@/components/ui/all-volumes/VolumesMetricsCards";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { siteConfig } from "../../siteConfig";
+
+const navigation = [
+  { name: "Overview", href: siteConfig.baseLinks.allvolumes.overview },
+  { name: "Trends", href: siteConfig.baseLinks.allvolumes.trends },
+  { name: "Breakdown", href: siteConfig.baseLinks.allvolumes.breakdown },
+];
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+  return (
+    <>
+      <div className="bg-white dark:bg-gray-925">
+        <div className="p-4 sm:p-2">
+          <VolumesMetricsCards />
+        </div>
+        <TabNavigation className="mt-6 gap-x-4 px-4 sm:px-6">
+          {navigation.map((item) => (
+            <TabNavigationLink
+              key={item.name}
+              asChild
+              active={pathname === item.href}
+            >
+              <Link href={item.href}>{item.name}</Link>
+            </TabNavigationLink>
+          ))}
+        </TabNavigation>
+        <>{children}</>
+      </div>
+    </>
+  );
+}
