1d57d0ef7b7c262492c6f71beb3d5a4934f0e57e
Ivan Molto <ivanmolto@gmail.com>
2025-07-07 12:30:33 +0200

    feat: Add Trends page to display daily, weekly, monthly, and quarterly trading volumes

---
+++ app/(dashboard)/all-volumes/trends/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Volumes from "@/components/ui/all-volumes/Volumes";
+export default function Trends() {
+  return (
+    <section aria-label="App Monitoring">
+      <dl className="-ml-4 grid grid-cols-1 gap-x-14 gap-y-10 border-t border-gray-200 p-6 md:grid-cols-2 dark:border-gray-800">
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Daily Trading Volume
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              Trading Volume over time grouped by day
+            </dd>
+          </div>
+          <Volumes slug="all-daily-volumes" />
+        </div>
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Weekly Trading Volume
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              Trading Volume over time grouped by week
+            </dd>
+          </div>
+          <Volumes slug="all-weekly-volumes" />
+        </div>
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Monthly Trading Volume
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              TradingVolume over time grouped by month
+            </dd>
+          </div>
+          <Volumes slug="all-monthly-volumes" />
+        </div>
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Quarterly Trading Volume
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              TradingVolume over time grouped by quarter
+            </dd>
+          </div>
+          <Volumes slug="all-quarterly-volumes" />
+        </div>
+      </dl>
+    </section>
+  );
+}
