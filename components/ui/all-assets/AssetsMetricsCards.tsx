c6d6d5ff00d6c7289268d71afc08a5d40d5dc564
Ivan Molto <ivanmolto@gmail.com>
2025-07-08 08:52:36 +0200

    feat: Add new components for displaying asset metrics including count, cumulative staked fees, and cumulative staked volumes

---
+++ components/ui/all-assets/AssetsMetricsCards.tsx
@@ -0,0 +1,19 @@
+import AssetsCount from "@/components/ui/all-assets/AssetsCount";
+
+export function AssetsMetricsCards() {
+  return (
+    <>
+      <h1 className="text-lg font-semibold text-gray-900 dark:text-gray-50">
+        Overview
+      </h1>
+      <dl className="mt-6 flex flex-wrap items-center gap-x-12 gap-y-8">
+        <div>
+          <dt className="text-sm text-gray-500 dark:text-gray-500">
+            Assets Count
+          </dt>
+          <AssetsCount slug="all-assets-count" />
+        </div>
+      </dl>
+    </>
+  );
+}
