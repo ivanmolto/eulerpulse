345344a3d994ed9c8c766d744605393d1d9303da
Ivan Molto <ivanmolto@gmail.com>
2025-07-08 08:53:12 +0200

    feat: Create Trends page for displaying daily, weekly, monthly, and quarterly trading volumes with AssetsStakedVolumes component

---
+++ app/(dashboard)/all-assets/volumes/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import AssetsStakedVolumes from "@/components/ui/all-assets/AssetsStakedVolumes";
+export default function Trends() {
+  return (
+    <section aria-label="App Monitoring">
+      <dl className="-ml-4 grid grid-cols-1 gap-x-14 gap-y-10 border-t border-gray-200 p-6 md:grid-cols-2 dark:border-gray-800">
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Daily Trading Volume
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              Trading Volume over time grouped by day and broken down by asset
+            </dd>
+          </div>
+          <AssetsStakedVolumes slug="daily-assets-volumes" />
+        </div>
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Weekly Trading Volume
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              Trading Volume over time grouped by week and broken down by asset
+            </dd>
+          </div>
+          <AssetsStakedVolumes slug="weekly-assets-volumes" />
+        </div>
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Monthly Trading Volume
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              Trading Volume over time grouped by month and broken down by asset
+            </dd>
+          </div>
+          <AssetsStakedVolumes slug="monthly-assets-vol" />
+        </div>
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Quarterly Trading Volume
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              Trading Volume over time grouped by quarter and broken down by
+              asset
+            </dd>
+          </div>
+          <AssetsStakedVolumes slug="quarterly-assets-volume" />
+        </div>
+      </dl>
+    </section>
+  );
+}
