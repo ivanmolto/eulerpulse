968912b9a3f4e44b622c39c50ad9d875bcfe7ab8
Ivan Molto <ivanmolto@gmail.com>
2025-07-08 02:35:26 +0200

    feat: Add Overview page for displaying cumulative trading volume on Ethereum

---
+++ app/(dashboard)/ethereum-volumes/overview/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import EthereumCumulativeVolumes from "@/components/ui/ethereum-volumes/EthereumCumulativeVolumes";
+import { cx } from "@/lib/utils";
+
+export default function Overview() {
+  return (
+    <section aria-labelledby="usage-overview">
+      <dl
+        className={cx(
+          "ml-2 mt-10 grid grid-cols-1 gap-14 sm:grid-cols-1 md:grid-cols-1 xl:grid-cols-1"
+        )}
+      >
+        <div className="flex flex-col justify-between p-0">
+          <div>
+            <dt className="text-sm font-semibold text-gray-900 dark:text-gray-50">
+              Cumulative Trading Volume over time on Ethereum
+            </dt>
+            <dd className="mt-0.5 text-sm/6 text-gray-500 dark:text-gray-500">
+              Trading Volume accumulated over time on Ethereum
+            </dd>
+          </div>
+          <EthereumCumulativeVolumes slug="ethereum-cumulative-volumes" />
+        </div>
+      </dl>
+    </section>
+  );
+}
