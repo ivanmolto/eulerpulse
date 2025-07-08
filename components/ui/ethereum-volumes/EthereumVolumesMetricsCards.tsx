28c066973e591208a514563d1800df34180d24fa
Ivan Molto <ivanmolto@gmail.com>
2025-07-08 02:38:28 +0200

    feat: Add Ethereum cumulative volumes and count components for displaying trading metrics

---
+++ components/ui/ethereum-volumes/EthereumVolumesMetricsCards.tsx
@@ -0,0 +1,17 @@
+import EthereumVolumesCount from "@/components/ui/ethereum-volumes/EthereumVolumesCount";
+
+export function EthereumVolumesMetricsCards() {
+  return (
+    <>
+      <h1 className="text-lg font-semibold text-gray-900 dark:text-gray-50">
+        Overview
+      </h1>
+      <dl className="mt-6 flex flex-wrap items-center gap-x-12 gap-y-8">
+        <div>
+          <dt className="text-sm text-gray-500 dark:text-gray-500">Volume</dt>
+          <EthereumVolumesCount slug="all-volumes-count" />
+        </div>
+      </dl>
+    </>
+  );
+}
