63a58ba6381c42089a045181a02c28e4618dd896
Ivan Molto <ivanmolto@gmail.com>
2025-07-10 20:07:43 +0200

    feat: Add new layout component for All Pools dashboard with tab navigation and metrics cards

---
+++ app/(dashboard)/all-pools/layout.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { TabNavigation, TabNavigationLink } from "@/components/TabNavigation";
+import { PoolsMetricsCards } from "@/components/ui/all-pools/PoolsMetricsCards";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { siteConfig } from "../../siteConfig";
+
+const navigation = [
+  { name: "Overview", href: siteConfig.baseLinks.allpools.overview },
+  { name: "Trading Volumes", href: siteConfig.baseLinks.allpools.trends },
+  /* { name: "Fees", href: siteConfig.baseLinks.allpools.breakdown }, */
+];
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+  return (
+    <>
+      <div className="bg-white dark:bg-gray-925">
+        <div className="p-4 sm:p-2">
+          <PoolsMetricsCards />
+        </div>
+        <TabNavigation className="mt-6 gap-x-4 px-4 sm:px-6">
+          {navigation.map((item) => (
+            <TabNavigationLink
+              key={item.name}
+              asChild
+              active={pathname === item.href}
+            >
+              <Link href={item.href}>{item.name}</Link>
+            </TabNavigationLink>
+          ))}
+        </TabNavigation>
+        <>{children}</>
+      </div>
+    </>
+  );
+}
